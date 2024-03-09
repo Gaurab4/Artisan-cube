2609075be652e59467d7a35a5a5784db472159c1
Gaurab4 <gauravthapa853@gmail.com>
2024-03-10 01:12:04 +0530

    Set up the basic file and added page auth for everypage

--- frontend/src/pages/boards/[projectname]/index.tsx
+++ frontend/src/pages/boards/[projectname]/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import BoardSideBar from '@/components/Sidebar/board_sidebar';
+import Link from 'next/link';
 
 const ProjectBoard = () => {
 
@@ -9,12 +11,10 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
   const [todoLists, setTodoLists] = useState([]);
   const router = useRouter();
   const { projectname, ProjectId , } = router.query; 
-  console.log(router.query);
-
   const handleAddTodoList =  async() => {
     try {
         const response = await axios.post(`${apiUrl}/todolist`,{
-            name:'Demo',
+            name: projectname,
             ProjectId:ProjectId,
         });
     } catch (error) {
@@ -23,16 +23,57 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
   };
 
   return (
-    <div>
-      <h1>Project: {projectname}</h1>
+    <div className='flex'>
+      <BoardSideBar/>
+      {/* <h1>Project: {projectname}</h1> */}
       
       {/* Button to add a todo list */}
-      <button
-        className="btn btn-primary"
-        onClick={() => setShowModal(true)}
-      >
-        Add Todo List
-      </button>
+
+    
+    <div className='mt-16 ml-10 '>
+    <div className="text-sm breadcrumbs">
+      <ul>
+      <li><Link href="/workspace">Workspace</Link></li>
+      {/* <li><Link href="/boards">Boards</Link></li> */}
+      <li>{projectname} board</li>
+      </ul>
+    </div>
+
+      <div className='flex'>
+        {/*  First List  permanent  */}
+        <div className='bg-slate-200 w-[200px]  h-[750px] rounded-lg mt-14 '>
+        {/* List Name  */}
+        <div className='p-4 flex justify-center'> 
+            Todo List 
+        </div>
+        <div className='  w-[200px]  max-h-[650px] overflow-x-auto'>
+
+      
+        </div>
+
+        {/* Add New  Task Button */}
+        <div
+          className="  btn bg-slate-200  shadow-none text-[15px] font-[200] w-[100%]"
+          onClick={() => setShowModal(true)}
+        >
+          ➕ New Task
+        </div>
+        </div>
+
+        {/* Other User Made Lists */}
+
+        <div className=' w-[250px] h-[50px] bg-slate-200 mt-14 ml-10 rounded-lg'>
+        <div className='flex justify-center   btn' onClick={() => setShowModal(true)}>
+          <p>➕ Add New List</p> 
+        </div>
+        
+          
+        </div> 
+      </div>
+
+
+    </div>
+     
 
       {/* Modal for adding a todo list */}
       {showModal && (
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,45 +1,11 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import React,{useState ,useEffect} from "react";
-import axios from 'axios';
-import CountdownTimer from "@/components/home_page/countdown-timer";
 import LaunchPage from "@/components/home_page/launch_page";
 
-interface User {
-    id:number;
-    name:string;
-    email:string;
-    password:string;
-}
-
 export default function Home() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
-  const [users , setUsers] = useState<User[]>([]);
-  const [newUser , setNewUser] = useState({name:'' , email: '', password:''});
-  
-  // fetch the users 
-
-    // useEffect(() => {
-    //   const fetchData = async () => {
-    //     try {
-    //       console.log('on trying...')
-    //       // const response =  await axios.get(`http://localhost:4000/users`);
-    //       const response = await axios.get(`${apiUrl}/users`);
-    //       setUsers(response.data);
-    //     } catch (error) {
-        
-    //       console.log('error fetchiing data ' , error);
-    //     }
-    //   };
-
-    //   fetchData();
-    // },[]);
 
     return (
       <main>
         <div>    
-              <LaunchPage/>  
-              {/* <CountdownTimer/> */}
+          <LaunchPage/>  
         </div>
       </main>
     )
